c3aeb837a80748159aa2769dbad3ba2a117071bc
Lars Willighagen <lars.willighagen@gmail.com>
2019-06-29 14:06:04 +0200

    [plugin:npm] Update tests

--- test/npm.js
+++ test/npm.js
@@ -8,9 +8,9 @@ export const apiTests = [
       author: [
         { family: 'Willighagen', given: 'Lars' }
       ],
-      issued: { 'date-parts': [[2019, 4, 13]] },
+      issued: { 'date-parts': [[2019, 6, 13]] },
       title: 'citation-js',
-      version: '0.4.0',
+      version: '0.4.5',
       type: 'book'
     }]
   }
